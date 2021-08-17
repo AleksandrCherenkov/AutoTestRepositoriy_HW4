c09eb7cd756ecbb0010a40f974f521fe5f7125d5
Aleksandr Cherenkov <cherenkov.a54@gmail.com>
2021-08-17 14:16:11 +0700

    С исправлениями по замечаниям !!!

--- src/test/java/mantis/tests/MantisViewIssuesTests.java
+++ src/test/java/mantis/tests/MantisViewIssuesTests.java
@@ -15,13 +15,9 @@ public class MantisViewIssuesTests {
 
     @Test
     public void countingViewIssuesTest() throws InterruptedException {
-//        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-
-//        WebDriverManager.operadriver().setup();
-//        WebDriver driver = new OperaDriver();
 
         driver.get("https://academ-it.ru/mantisbt/login_page.php");
         driver.manage().window().maximize();
@@ -33,17 +29,10 @@ public class MantisViewIssuesTests {
         driver.findElement(By.cssSelector("#password")).sendKeys("admin20");
         driver.findElement(By.cssSelector("input[type='submit']")).click();
 
-        driver.findElement(By.xpath("//div[@id='sidebar']/ul/li[2]/a/span")).click();
-
-//        driver.findElement(By.cssSelector("a[href='/mantisbt/view_all_bug_page.php']")).click();
+        driver.findElement(By.cssSelector("a[href='/mantisbt/view_all_bug_page.php']")).click();
 
         List<WebElement> list = driver.findElements(By.xpath("//table[@id='buglist']/tbody/tr"));
         Assertions.assertEquals(50, list.size());
-
-//        List<WebElement> list = driver.findElements(By.cssSelector("#buglist tr"));
-//        Assertions.assertEquals(51, list.size()); // Считает повидимому вместе со строкой названий столбцов!!!
-
-        Thread.sleep(2311);
 
         driver.findElement(By.xpath("//div[@id='navbar-container']/div/a/span")).click();
 
